250177fbe64ac25e619b12664a1f18e8a0effa4f
Agustina Barbetta <agustina.barbetta@gmail.com>
2015-08-09 23:44:45 -0300

    Code to control a servo with a potentiometer

---
+++ potentiometeredServo/potentiometeredServo.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo servo;
+const int servoPin = 9;
+
+const int potentiometerPin = A0;
+int potentiometerValue;
+int angle;
+
+void setup() {
+  
+  servo.attach(servoPin);
+  
+  // Serial connection to check values from the potentiometer
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  potentiometerValue = analogRead(potentiometerPin);
+  
+  Serial.print("potentiometer: ");
+  Serial.print(potentiometerValue);
+  
+  // Change value between 0-1023 to values between 0-179
+  angle = map(potentiometerValue, 0, 1023, 0, 179);
+  
+  Serial.print(", angle: ");
+  Serial.println(angle);
+  
+  servo.write(angle);
+  delay(15);
+  
+}
+
